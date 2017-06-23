8ba33541bd0f79a62728750ebb7403f9ad3f0070
borisdj <djurdjevicboris@hotmail.com>
2017-06-23 19:10:13 +0200

    TestExample.sql updated

--- EFCore.BulkExtensions.Tests/TestExample.sql
+++ EFCore.BulkExtensions.Tests/TestExample.sql
@@ -12,25 +12,25 @@ LEFT JOIN dbo.[Item] AS Source ON 1 = 0;
 
 -- Insert into TempTable
 INSERT INTO [ItemTemp1234]
-(ItemId, Description, Name, Price, Quantity, TimeUpdated)
+([ItemId], [Description], [Name], [Price], [Quantity], [TimeUpdated])
 Values
 (1, 'Desc1', 'SomeName1', 22.11, 34, '2017-01-01'),
 (2, 'Desc2', 'SomeName2', 12.66, 45, '2017-01-01'),
 (3, 'Desc3', 'SomeName3', 14.35, 46, '2017-01-01');
 
 -- Insert/Update with MERGE from TempTable
-MERGE dbo.[Item] WITH (HOLDLOCK) AS T USING (SELECT TOP 2147483647 * FROM dbo.[ItemTemp1234] ORDER BY ItemId) AS S
-ON T.ItemId = S.ItemId
+MERGE dbo.[Item] WITH (HOLDLOCK) AS T USING (SELECT TOP 2147483647 * FROM dbo.[ItemTemp1234] ORDER BY [ItemId]) AS S
+ON T.[ItemId] = S.[ItemId]
 WHEN NOT MATCHED THEN INSERT
-(Description, Name, Price, Quantity, TimeUpdated)
+([Description], [Name], [Price], [Quantity], [TimeUpdated])
 VALUES
-(S.Description, S.Name, S.Price, S.Quantity, S.TimeUpdated)
+(S.[Description], S.[Name], S.[Price], S.[Quantity], S.[TimeUpdated])
 WHEN MATCHED THEN UPDATE SET
-T.Description = S.Description,
-T.Name = S.Name,
-T.Price = S.Price,
-T.Quantity = S.Quantity,
-T.TimeUpdated = S.TimeUpdated
+T.[Description] = S.[Description],
+T.[Name] = S.[Name],
+T.[Price] = S.[Price],
+T.[Quantity] = S.[Quantity],
+T.[TimeUpdated] = S.[TimeUpdated]
 OUTPUT inserted.* INTO dbo.[ItemTemp1234Output];
 
 -- Delete TempTable
