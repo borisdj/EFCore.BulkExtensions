aefa66955dfe6737ef31953253d23547adba1740
Boris Djurdjevic <djurdjevicboris@hotmail.com>
2018-06-24 23:21:05 +0200

    Update SqlBulkOperation.cs
    Removed redundant 'using'

--- EFCore.BulkExtensions/SqlBulkOperation.cs
+++ EFCore.BulkExtensions/SqlBulkOperation.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
-using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Storage;
