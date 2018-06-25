fb4fb6390aa3bb8b03143ac2db186723ee40b2e3
borisdj <djurdjevicboris@hotmail.com>
2018-06-25 09:39:23 +0200

    Test updated

--- EFCore.BulkExtensions.Tests/EFCoreBulkTest.cs
+++ EFCore.BulkExtensions.Tests/EFCoreBulkTest.cs
@@ -9,19 +9,18 @@ namespace EFCore.BulkExtensions.Tests
 {
     public class EFCoreBulkTest
     {
-        protected int EntitiesNumber => 100000;
+        protected int EntitiesNumber => 1000;
 
         [Theory]
-        //[InlineData(true)]
-        [InlineData(true, true)]
-        //[InlineData(false, true)] // for speed comparison with Regular EF CUD operations
-        public void OperationsTest(bool isBulkOperation, bool insertTo2Tables = false)
+        [InlineData(true)]
+        //[InlineData(false)] // for speed comparison with Regular EF CUD operations
+        public void OperationsTest(bool isBulkOperation)
         {
             // Test can be run individually by commenting others and running each separately in order one after another
-            RunInsert(isBulkOperation, insertTo2Tables);
-            RunInsertOrUpdate(isBulkOperation);
+            RunInsert(isBulkOperation);
+            /*RunInsertOrUpdate(isBulkOperation);
             RunUpdate(isBulkOperation);
-            RunDelete(isBulkOperation);
+            RunDelete(isBulkOperation);*/
         }
 
         private void WriteProgress(decimal percentage)
@@ -29,7 +28,7 @@ namespace EFCore.BulkExtensions.Tests
             Debug.WriteLine(percentage);
         }
 
-        private void RunInsert(bool isBulkOperation, bool insertTo2Tables = false)
+        private void RunInsert(bool isBulkOperation)
         {
             using (var context = new TestContext(ContextUtil.GetOptions()))
             {
@@ -37,67 +36,69 @@ namespace EFCore.BulkExtensions.Tests
                 var subEntities = new List<ItemHistory>();
                 for (int i = 1; i < EntitiesNumber; i++)
                 {
-                    entities.Add(new Item
+                    var entity = new Item
                     {
                         ItemId = isBulkOperation ? i : 0,
                         Name = "name " + i,
                         Description = "info " + Guid.NewGuid().ToString().Substring(0, 3),
                         Quantity = i % 10,
                         Price = i / (i % 5 + 1),
-                        TimeUpdated = DateTime.Now
-                    });
-                }
-                if (insertTo2Tables)
-                {
-                    foreach (var entity in entities)
+                        TimeUpdated = DateTime.Now,
+                        ItemHistories = new List<ItemHistory>()
+                    };
+
+                    var subEntity1 = new ItemHistory
                     {
-                        subEntities.Add(new ItemHistory
-                        {
-                            ItemHistoryId = SeqGuid.Create(),
-                            ItemId = entity.ItemId,
-                            Remark = "some more info"
-                        });
-                    }
+                        ItemHistoryId = SeqGuid.Create(),
+                        Remark = $"some more info {i}.1"
+                    };
+                    var subEntity2 = new ItemHistory
+                    {
+                        ItemHistoryId = SeqGuid.Create(),
+                        Remark = $"some more info {i}.2"
+                    };
+                    entity.ItemHistories.Add(subEntity1);
+                    entity.ItemHistories.Add(subEntity2);
+
+                    entities.Add(entity);
                 }
 
                 if (isBulkOperation)
                 {
-                    if (!insertTo2Tables)
+                    using (var transaction = context.Database.BeginTransaction())
                     {
-                        context.BulkInsert(entities);
-                    }
-                    else
-                    {
-                        using (var transaction = context.Database.BeginTransaction())
+                        context.BulkInsert(
+                            entities,
+                            new BulkConfig
+                            {
+                                PreserveInsertOrder = true,
+                                SetOutputIdentity = true,
+                                BatchSize = 4000,
+                                UseTempDB = true
+                            },
+                            (a) => WriteProgress(a)
+                        );
+
+                        foreach (var entity in entities)
                         {
-                            context.BulkInsert(
-                                entities,
-                                new BulkConfig
-                                {
-                                    PreserveInsertOrder = true,
-                                    SetOutputIdentity = true,
-                                    BatchSize = 4000,
-                                    UseTempDB = true
-                                },
-                                (a) => WriteProgress(a)
-                            );
-                            context.BulkInsert(subEntities);
-                            transaction.Commit();
+                            foreach (var subEntity in entity.ItemHistories)
+                            {
+                                subEntity.ItemId = entity.ItemId; // setting FK to match its linked PK that was generated in DB
+                            }
+                            subEntities.AddRange(entity.ItemHistories);
                         }
+                        context.BulkInsert(subEntities);
+
+                        transaction.Commit();
                     }
                 }
                 else
                 {
                     context.Items.AddRange(entities);
-
-                    if (insertTo2Tables)
-                    {
-                        context.ItemHistories.AddRange(subEntities);
-                    }
-
                     context.SaveChanges();
                 }
             }
+
             using (var context = new TestContext(ContextUtil.GetOptions()))
             {
                 int entitiesCount = context.Items.Count();
