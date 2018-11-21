002ae81c5ab0b73bf52400786fff37c3588bcc38
borisdj <djurdjevicboris@hotmail.com>
2018-11-21 11:27:42 +0100

    On BatchExtensions added to return number affected

--- EFCore.BulkExtensions/IQueryableBatchExtensions.cs
+++ EFCore.BulkExtensions/IQueryableBatchExtensions.cs
@@ -7,34 +7,34 @@ namespace EFCore.BulkExtensions
 {
     public static class IQueryableBatchExtensions
     {
-        public static void BatchDelete<T>(this IQueryable<T> query) where T : class, new()
+        public static int BatchDelete<T>(this IQueryable<T> query) where T : class, new()
         {
             DbContext context = BatchUtil.GetDbContext(query);
             string sql = BatchUtil.GetSqlDelete(query, context);
-            context.Database.ExecuteSqlCommand(sql);
+            return context.Database.ExecuteSqlCommand(sql);
         }
 
-        public static void BatchUpdate<T>(this IQueryable<T> query, T updateValues, List<string> updateColumns = null) where T : class, new()
+        public static int BatchUpdate<T>(this IQueryable<T> query, T updateValues, List<string> updateColumns = null) where T : class, new()
         {
             DbContext context = BatchUtil.GetDbContext(query);
             string sql = BatchUtil.GetSqlUpdate(query, context, updateValues, updateColumns);
-            context.Database.ExecuteSqlCommand(sql);
+            return context.Database.ExecuteSqlCommand(sql);
         }
 
         // Async methods
 
-        public static async Task BatchDeleteAsync<T>(this IQueryable<T> query) where T : class, new()
+        public static async Task<int> BatchDeleteAsync<T>(this IQueryable<T> query) where T : class, new()
         {
             DbContext context = BatchUtil.GetDbContext(query);
             string sql = BatchUtil.GetSqlDelete(query, context);
-            await context.Database.ExecuteSqlCommandAsync(sql);
+            return await context.Database.ExecuteSqlCommandAsync(sql);
         }
 
-        public static async Task BatchUpdateAsync<T>(this IQueryable<T> query, T updateValues, List<string> updateColumns = null) where T : class, new()
+        public static async Task<int> BatchUpdateAsync<T>(this IQueryable<T> query, T updateValues, List<string> updateColumns = null) where T : class, new()
         {
             DbContext context = BatchUtil.GetDbContext(query);
             string sql = BatchUtil.GetSqlUpdate(query, context, updateValues, updateColumns);
-            await context.Database.ExecuteSqlCommandAsync(sql);
+            return await context.Database.ExecuteSqlCommandAsync(sql);
         }
     }
 }
