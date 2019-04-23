dbb9791eb4016387c2aad4ffd18f2f51c654d847
Vladimir Horbovanu <vladhorby@gmail.com>
2019-04-23 13:11:01 -0500

    Solves issue #177 Batch operations: Inner query parameters are passed in the clear

---
+++ EFCore.BulkExtensions/SimpleParameterValues.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+
+namespace EFCore.BulkExtensions
+{
+	internal class SimpleParameterValues: IParameterValues
+	{
+		private readonly Dictionary<string, object> _parameterValues = new Dictionary<string, object>();
+
+		public SimpleParameterValues()
+		{
+			
+		}
+		public void AddParameter(string name, object value )
+		{
+			_parameterValues.Add(name, value);
+		}
+
+		public object RemoveParameter(string name)
+		{
+			_parameterValues.TryGetValue(name, out var val);
+			_parameterValues.Remove(name);
+			return val;
+		}
+
+		public void SetParameter(string name, object value)
+		{
+			_parameterValues[name] = value;
+		}
+
+		public IReadOnlyDictionary<string, object> ParameterValues => new ReadOnlyDictionary<string, object>(_parameterValues);
+	
+	}
+}
