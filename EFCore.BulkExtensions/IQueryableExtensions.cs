ea7ef290c9e1481405b90de219f02e4f5eff0f9f
borisdj <djurdjevicboris@hotmail.com>
2019-12-18 17:48:38 +0100

    NuGet 3.1.0

--- EFCore.BulkExtensions/IQueryableExtensions.cs
+++ EFCore.BulkExtensions/IQueryableExtensions.cs
@@ -18,33 +18,43 @@ namespace EFCore.BulkExtensions
 
         public static (string, IEnumerable<SqlParameter>) ToParametrizedSql<TEntity>(this IQueryable<TEntity> query) where TEntity : class
         {
+            string relationalCommandCacheText = "_relationalCommandCache";
+            string selectExpressionText = "_selectExpression";
+            string querySqlGeneratorFactoryText = "_querySqlGeneratorFactory";
+            string relationalQueryContextText = "_relationalQueryContext";
+
+            string cannotGetText = "Cannot get";
+
             var enumerator = query.Provider.Execute<IEnumerable<TEntity>>(query.Expression).GetEnumerator();
-            var relationalCommandCache = enumerator.Private("_relationalCommandCache");
+            var relationalCommandCache = enumerator.Private(relationalCommandCacheText);
+
+
 
             SelectExpression selectExpression;
             IQuerySqlGeneratorFactory factory;
             if (relationalCommandCache != null)
             {
-                selectExpression = relationalCommandCache.Private<SelectExpression>("_selectExpression") ?? throw new InvalidOperationException($"Cannot get _selectExpression");
-                factory = relationalCommandCache.Private<IQuerySqlGeneratorFactory>("_querySqlGeneratorFactory") ?? throw new InvalidOperationException($"Cannot get _querySqlGeneratorFactory");
+                selectExpression = relationalCommandCache.Private<SelectExpression>(selectExpressionText) ?? throw new InvalidOperationException($"{cannotGetText} {selectExpressionText}");
+                factory = relationalCommandCache.Private<IQuerySqlGeneratorFactory>(querySqlGeneratorFactoryText) ?? throw new InvalidOperationException($"{cannotGetText} {querySqlGeneratorFactoryText}");
             }
             else
             {
-                selectExpression = enumerator.Private<SelectExpression>("_selectExpression") ?? throw new InvalidOperationException($"Cannot get _selectExpression");
-                factory = enumerator.Private<IQuerySqlGeneratorFactory>("_querySqlGeneratorFactory") ?? throw new InvalidOperationException($"Cannot get _querySqlGeneratorFactory");
+                selectExpression = enumerator.Private<SelectExpression>(selectExpressionText) ?? throw new InvalidOperationException($"{cannotGetText} {selectExpressionText}");
+                factory = enumerator.Private<IQuerySqlGeneratorFactory>(querySqlGeneratorFactoryText) ?? throw new InvalidOperationException($"{cannotGetText} {querySqlGeneratorFactoryText}");
             }
 
             var sqlGenerator = factory.Create();
             var command = sqlGenerator.GetCommand(selectExpression);
 
-            var queryContext = enumerator.Private<RelationalQueryContext>("_relationalQueryContext") ?? throw new InvalidOperationException($"Cannot get RelationalQueryContext");
+            var queryContext = enumerator.Private<RelationalQueryContext>(relationalQueryContextText) ?? throw new InvalidOperationException($"{cannotGetText} {relationalQueryContextText}");
             SqlParameter[] parameters = queryContext.ParameterValues.Select(a => new SqlParameter("@" + a.Key, a.Value)).ToArray();
 
             string sql = command.CommandText;
             return (sql, parameters);
         }
 
-        private static object Private(this object obj, string privateField) => obj?.GetType().GetField(privateField, BindingFlags.Instance | BindingFlags.NonPublic)?.GetValue(obj);
-        private static T Private<T>(this object obj, string privateField) => (T)obj?.GetType().GetField(privateField, BindingFlags.Instance | BindingFlags.NonPublic)?.GetValue(obj);
+        private static readonly BindingFlags bindingFlags = BindingFlags.Instance | BindingFlags.NonPublic;
+        private static object Private(this object obj, string privateField) => obj?.GetType().GetField(privateField, bindingFlags)?.GetValue(obj);
+        private static T Private<T>(this object obj, string privateField) => (T)obj?.GetType().GetField(privateField, bindingFlags)?.GetValue(obj);
     }
 }
