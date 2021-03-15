146e63573b2c8377ab1685d25245556b5d649ad4
maitlandmarshall <maitlandmarshallapps@gmail.com>
2021-03-15 10:12:20 +1000

    Implement isAsync flag to avoid code duplication

--- EFCore.BulkExtensions/DbContextBulkTransactionGraphUtil.cs
+++ EFCore.BulkExtensions/DbContextBulkTransactionGraphUtil.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -13,11 +14,17 @@ namespace EFCore.BulkExtensions
     internal static class DbContextBulkTransactionGraphUtil
     {
         public static void ExecuteWithGraph(DbContext context, IEnumerable<object> entities, OperationType operationType, BulkConfig bulkConfig, Action<decimal> progress)
+            => ExecuteWithGraphAsync_Impl(context, entities, operationType, bulkConfig, progress, null, isAsync: false).GetAwaiter().GetResult();
+
+        public static Task ExecuteWithGraphAsync(DbContext context, IEnumerable<object> entities, OperationType operationType, BulkConfig bulkConfig, Action<decimal> progress, CancellationToken cancellationToken)
+            => ExecuteWithGraphAsync_Impl(context, entities, operationType, bulkConfig, progress, cancellationToken, isAsync: true);
+
+        private static async Task ExecuteWithGraphAsync_Impl(DbContext context, IEnumerable<object> entities, OperationType operationType, BulkConfig bulkConfig, Action<decimal> progress, CancellationToken? cancellationToken, bool isAsync = true)
         {
             if (operationType != OperationType.Insert
-                        && operationType != OperationType.InsertOrUpdate
-                        && operationType != OperationType.InsertOrUpdateDelete
-                        && operationType != OperationType.Update)
+                       && operationType != OperationType.InsertOrUpdate
+                       && operationType != OperationType.InsertOrUpdateDelete
+                       && operationType != OperationType.Update)
                 throw new InvalidBulkConfigException($"{nameof(BulkConfig)}.{nameof(BulkConfig.IncludeGraph)} only supports Insert or Update operations.");
 
             // If this is set to false, won't be able to propogate new primary keys to the relationships
@@ -28,96 +35,81 @@ namespace EFCore.BulkExtensions
             if (rootGraphItems == null)
                 return;
 
-            foreach (var actionGraphItem in rootGraphItems)
+            // Inserting an entity graph must be done within a transaction otherwise the database could end up in a bad state
+            var hasExistingTransaction = context.Database.CurrentTransaction != null;
+            var transaction = context.Database.CurrentTransaction ?? (isAsync ? await context.Database.BeginTransactionAsync() : context.Database.BeginTransaction());
+
+            try
             {
-                var entitiesToAction = actionGraphItem.Entities.Select(y => y.Entity).ToList();
-                var tableInfo = TableInfo.CreateInstance(context, actionGraphItem.EntityClrType, entitiesToAction, operationType, bulkConfig);
-
-                SqlBulkOperation.Merge(context, actionGraphItem.EntityClrType, entitiesToAction, tableInfo, operationType, progress);
-
-                // Loop through the dependants and update their foreign keys with the PK values of the just inserted / merged entities
-                foreach (var graphEntity in actionGraphItem.Entities)
+                foreach (var actionGraphItem in rootGraphItems)
                 {
-                    var entity = graphEntity.Entity;
-                    var parentEntity = graphEntity.ParentEntity;
+                    var entitiesToAction = actionGraphItem.Entities.Select(y => y.Entity).ToList();
+                    var tableInfo = TableInfo.CreateInstance(context, actionGraphItem.EntityClrType, entitiesToAction, operationType, bulkConfig);
 
-                    // If the parent entity is null its the root type of the object graph.
-                    if (parentEntity is null)
+                    if (isAsync)
                     {
-                        continue;
+                        await SqlBulkOperation.MergeAsync(context, actionGraphItem.EntityClrType, entitiesToAction, tableInfo, operationType, progress, cancellationToken.Value);
                     }
                     else
                     {
-                        var navigation = actionGraphItem.ParentNavigation;
+                        SqlBulkOperation.Merge(context, actionGraphItem.EntityClrType, entitiesToAction, tableInfo, operationType, progress);
+                    }
+                    
 
-                        if (navigation.IsDependentToPrincipal())
+                    // Loop through the dependants and update their foreign keys with the PK values of the just inserted / merged entities
+                    foreach (var graphEntity in actionGraphItem.Entities)
+                    {
+                        var entity = graphEntity.Entity;
+                        var parentEntity = graphEntity.ParentEntity;
+
+                        // If the parent entity is null its the root type of the object graph.
+                        if (parentEntity is null)
                         {
-                            SetForeignKeyForRelationship(context, navigation,
-                                dependent: parentEntity,
-                                principal: entity);
+                            continue;
                         }
                         else
                         {
-                            SetForeignKeyForRelationship(context, navigation,
-                                dependent: entity,
-                                principal: parentEntity);
+                            var navigation = actionGraphItem.ParentNavigation;
+
+                            if (navigation.IsDependentToPrincipal())
+                            {
+                                SetForeignKeyForRelationship(context, navigation,
+                                    dependent: parentEntity,
+                                    principal: entity);
+                            }
+                            else
+                            {
+                                SetForeignKeyForRelationship(context, navigation,
+                                    dependent: entity,
+                                    principal: parentEntity);
+                            }
                         }
                     }
                 }
-            }
-        }
 
-
-        public static async Task ExecuteWithGraphAsync(DbContext context, IEnumerable<object> entities, OperationType operationType, BulkConfig bulkConfig, Action<decimal> progress, CancellationToken cancellationToken)
-        {
-            if (operationType != OperationType.Insert
-                        && operationType != OperationType.InsertOrUpdate
-                        && operationType != OperationType.InsertOrUpdateDelete
-                        && operationType != OperationType.Update)
-                throw new InvalidBulkConfigException($"{nameof(BulkConfig)}.{nameof(BulkConfig.IncludeGraph)} only supports Insert or Update operations.");
-
-            // If this is set to false, won't be able to propogate new primary keys to the relationships
-            bulkConfig.SetOutputIdentity = true;
-
-            var rootGraphItems = GraphUtil.GetOrderedGraph(context, entities);
-
-            if (rootGraphItems == null)
-                return;
-
-            foreach (var actionGraphItem in rootGraphItems)
-            {
-                var entitiesToAction = actionGraphItem.Entities.Select(y => y.Entity).ToList();
-                var tableInfo = TableInfo.CreateInstance(context, actionGraphItem.EntityClrType, entitiesToAction, operationType, bulkConfig);
-
-                await SqlBulkOperation.MergeAsync(context, actionGraphItem.EntityClrType, entitiesToAction, tableInfo, operationType, progress, cancellationToken);
-
-                // Loop through the dependants and update their foreign keys with the PK values of the just inserted / merged entities
-                foreach (var graphEntity in actionGraphItem.Entities)
+                if (hasExistingTransaction == false)
                 {
-                    var entity = graphEntity.Entity;
-                    var parentEntity = graphEntity.ParentEntity;
-
-                    // If the parent entity is null its the root type of the object graph.
-                    if (parentEntity is null)
+                    if (isAsync)
                     {
-                        continue;
+                        await transaction.CommitAsync();
                     }
                     else
                     {
-                        var navigation = actionGraphItem.ParentNavigation;
-
-                        if (navigation.IsDependentToPrincipal())
-                        {
-                            SetForeignKeyForRelationship(context, navigation,
-                                dependent: parentEntity,
-                                principal: entity);
-                        }
-                        else
-                        {
-                            SetForeignKeyForRelationship(context, navigation,
-                                dependent: entity,
-                                principal: parentEntity);
-                        }
+                        transaction.Commit();
+                    }
+                }
+            }
+            finally
+            {
+                if (hasExistingTransaction == false)
+                {
+                    if (isAsync)
+                    {
+                        await transaction.DisposeAsync();
+                    }
+                    else
+                    {
+                        transaction.Dispose();
                     }
                 }
             }
