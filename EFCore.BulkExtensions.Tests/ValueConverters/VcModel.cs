65fa16f75a3e8317154e88bfc1bc1e51ec18acd1
Jurriaan Wijnberg <jurriaan.wijnberg@visma.com>
2021-03-22 10:32:04 +0100

    Fix BatchDelete and BatchUpdate with where expression and ValueConverter

--- EFCore.BulkExtensions.Tests/ValueConverters/VcModel.cs
+++ EFCore.BulkExtensions.Tests/ValueConverters/VcModel.cs
@@ -9,6 +9,8 @@ namespace EFCore.BulkExtensions.Tests.ValueConverters
         public int Id { get; set; }
 
         public VcEnum Enum { get; set; }
+
+        public LocalDate LocalDate { get; set; }
     }
 
     public enum VcEnum
@@ -16,5 +18,29 @@ namespace EFCore.BulkExtensions.Tests.ValueConverters
         Why,
         Hello,
         There
+    }
+
+    public readonly struct LocalDate
+    {
+        public LocalDate(int year, int month, int day)
+        {
+            this.Year = year;
+            this.Month = month;
+            this.Day = day;
+        }
+
+        public readonly int Year;
+        public readonly int Month;
+        public readonly int Day;
+
+        public static bool operator >(LocalDate lhs, LocalDate rhs)
+        {
+            return false;
+        }
+
+        public static bool operator <(LocalDate lhs, LocalDate rhs)
+        {
+            return false;
+        }
     }
 }
