99658ee153ca088d3cc3084eb6f5a8a265b3a645
Fernando De Mennato <fdemennato@progesoftware.it>
2021-07-08 18:21:50 +0200

    If the input have a value for property that have HasDefaultValueSql, don't skip this property in insert state

---
+++ EFCore.BulkExtensions/Helpers/GenericsHelpers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace EFCore.BulkExtensions.Helpers
+{
+    internal static class GenericsHelpers
+    {
+
+        internal static IEnumerable<string> GetPropertiesDefaultValue<T>(this T value) where T : class
+        {
+            Type type = typeof(T);
+            // Obtain all fields with type pointer.
+            PropertyInfo[] arrayPropertyInfos = type.GetProperties();
+            var result = new List<string>();
+            foreach (var field in arrayPropertyInfos)
+            {
+                string name = field.Name;
+                object temp = field.GetValue(value);
+                if (temp == default) result.Add(name);
+                if(temp is Guid && (Guid)temp == Guid.Empty) result.Add(name);
+            }
+
+            return result;
+        }
+
+        internal static IEnumerable<string> GetPropertiesWithDefaultValue<T>(this IEnumerable<T> values) where T : class
+        {
+            var result = values.SelectMany(x => x.GetPropertiesDefaultValue()).ToList().Distinct();
+
+            return result;    
+        }
+
+    }
+}
