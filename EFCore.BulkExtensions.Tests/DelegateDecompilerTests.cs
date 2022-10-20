94bcf6b803921125234de2e6412e3d6dda4710b1
Carl Meyertons <carl.meyertons@pwc.com>
2022-10-20 09:31:52 -0500

    Adding Delegate Decompiler support during Batch operations

---
+++ EFCore.BulkExtensions.Tests/DelegateDecompilerTests.cs
@@ -0,0 +1,20 @@
+﻿using DelegateDecompiler.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EFCore.BulkExtensions.Tests;
+public class DelegateDecompilerTests
+{
+    [Fact]
+    public async Task DelegateDecompiler_DecompileAsync_WorksAsync()
+    {
+        using var context = new TestContext(ContextUtil.GetOptions());
+
+        await context.Items
+            .Where(x => x.ItemId < 0)
+            .DecompileAsync()
+            .BatchDeleteAsync()
+        ;
+    }
+}
