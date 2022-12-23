c46af6452a4873efc176c33b9b9a41e9c40fc5f1
Dion Leurink <dion@onlineplasticsgroup.com>
2022-12-23 16:52:23 +0100

    fix: comments/cleanup

--- EFCore.BulkExtensions/Helpers/QuerySqlGeneratorHelpers.cs
+++ EFCore.BulkExtensions/Helpers/QuerySqlGeneratorHelpers.cs
@@ -8,9 +8,19 @@ namespace EFCore.BulkExtensions.Helpers;
 
 internal static class QuerySqlGeneratorHelpers
 {
-
+    /// <summary>
+    /// Cached delegate.
+    /// For more info see: https://codeblog.jonskeet.uk/2008/08/09/making-reflection-fly-and-exploring-delegates/
+    /// </summary>
     private static Func<QuerySqlGenerator, SelectExpression, IRelationalCommand>? _getCommandFunc;
 
+    /// <summary>
+    /// Retrieves a <see cref="IRelationalCommand"/> using <see cref="QuerySqlGenerator.GetCommand"/>,
+    /// But also applies a fix when used with EF7
+    /// </summary>
+    /// <param name="querySqlGenerator"></param>
+    /// <param name="selectExpression"></param>
+    /// <returns></returns>
     public static IRelationalCommand GetCommand(QuerySqlGenerator querySqlGenerator, SelectExpression selectExpression)
     {
         if (_getCommandFunc == null)
@@ -25,8 +35,8 @@ internal static class QuerySqlGeneratorHelpers
 
     private static void InitGetCommand()
     {
-        // EF6 uses GetCommand(SelectExpression)
-        // EF7 uses GetCommand(Expression)
+        // EF6 uses QuerySqlGenerator.GetCommand(SelectExpression)
+        // EF7 uses QuerySqlGenerator.GetCommand(Expression)
         // See: https://github.com/dotnet/efcore/blob/3f82c2b0af132b7019f6bd8b32c2709469b0ffae/src/EFCore.Relational/Query/QuerySqlGenerator.cs#L67
         var methodInfo = typeof(QuerySqlGenerator)
             .GetMethod(nameof(QuerySqlGenerator.GetCommand), new[] { typeof(Expression) });
