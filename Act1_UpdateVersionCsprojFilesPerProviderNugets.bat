be53da56f43b1aff0b2c14968a9d0fe8c1aa5e35
Boris Djurdjevic <djurdjevicboris@hotmail.com>
2023-07-03 15:56:22 +0200

    version upgrade 7.1.3

---
+++ Act1_UpdateVersionCsprojFilesPerProviderNugets.bat
@@ -0,0 +1,65 @@
+@echo off 
+    setlocal enableextensions disabledelayedexpansion
+	
+    set "search=^>7.1.2"
+    set "replace=^>7.1.3"
+	
+	set "search2=Multiple fixes"
+    set "replace2=Some updates"
+	
+    set "textFile1=EFCore.BulkExtensions.PerProvider\EFCore.BulkExtensions.MySql\EFCore.BulkExtensions.MySql.csproj"
+	set "textFile2=EFCore.BulkExtensions.PerProvider\EFCore.BulkExtensions.PostgreSql\EFCore.BulkExtensions.PostgreSql.csproj"
+	set "textFile3=EFCore.BulkExtensions.PerProvider\EFCore.BulkExtensions.Sqlite\EFCore.BulkExtensions.Sqlite.csproj"
+	set "textFile4=EFCore.BulkExtensions.PerProvider\EFCore.BulkExtensions.SqlServer\EFCore.BulkExtensions.SqlServer.csproj"
+	
+    for /f "delims=" %%i in ('type "%textFile1%" ^& break ^> "%textFile1%" ') do (
+        set "line=%%i"
+        setlocal enabledelayedexpansion
+        >>"%textFile1%" echo(!line:%search%=%replace%!
+        endlocal
+    )
+	for /f "delims=" %%i in ('type "%textFile1%" ^& break ^> "%textFile1%" ') do (
+        set "line=%%i"
+        setlocal enabledelayedexpansion
+        >>"%textFile1%" echo(!line:%search2%=%replace2%!
+        endlocal
+    )
+	
+    for /f "delims=" %%i in ('type "%textFile2%" ^& break ^> "%textFile2%" ') do (
+        set "line=%%i"
+        setlocal enabledelayedexpansion
+        >>"%textFile2%" echo(!line:%search%=%replace%!
+        endlocal
+    )	
+    for /f "delims=" %%i in ('type "%textFile2%" ^& break ^> "%textFile2%" ') do (
+        set "line=%%i"
+        setlocal enabledelayedexpansion
+        >>"%textFile2%" echo(!line:%search2%=%replace2%!
+        endlocal
+    )
+	
+	for /f "delims=" %%i in ('type "%textFile3%" ^& break ^> "%textFile3%" ') do (
+        set "line=%%i"
+        setlocal enabledelayedexpansion
+        >>"%textFile3%" echo(!line:%search%=%replace%!
+        endlocal
+    )
+	for /f "delims=" %%i in ('type "%textFile3%" ^& break ^> "%textFile3%" ') do (
+        set "line=%%i"
+        setlocal enabledelayedexpansion
+        >>"%textFile3%" echo(!line:%search2%=%replace2%!
+        endlocal
+    )
+	
+    for /f "delims=" %%i in ('type "%textFile4%" ^& break ^> "%textFile4%" ') do (
+        set "line=%%i"
+        setlocal enabledelayedexpansion
+        >>"%textFile4%" echo(!line:%search%=%replace%!
+        endlocal
+    )	
+    for /f "delims=" %%i in ('type "%textFile4%" ^& break ^> "%textFile4%" ') do (
+        set "line=%%i"
+        setlocal enabledelayedexpansion
+        >>"%textFile4%" echo(!line:%search2%=%replace2%!
+        endlocal
+    )	
--- EFCore.BulkExtensions/SqlAdapters/ISqlOperationsAdapter.cs
+++ EFCore.BulkExtensions/SqlAdapters/ISqlOperationsAdapter.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Npgsql;
 using System;
 using System.Collections.Generic;
 using System.Threading;
