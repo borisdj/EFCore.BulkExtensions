45f9b84476eb60e5d2b6601e7cdcb8a6721d5743
Jelle Kerkstra <j.kerkstra@viscon.eu>
2024-11-14 15:55:30 +0100

    Add TableInfoTests.CreateTableInfo_For_Hidden_PrimaryKey_Does_Not_Throw

---
+++ EFCore.BulkExtensions.Tests/TableInfoTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using EFCore.BulkExtensions.SqlAdapters;
+using Xunit;
+
+namespace EFCore.BulkExtensions.Tests;
+
+public class TableInfoTests
+{
+    [Theory]
+    [InlineData(SqlType.SqlServer)]
+    private void CreateTableInfo_For_Hidden_PrimaryKey_Does_Not_Throw(SqlType sqlType)
+    {
+        ContextUtil.DatabaseType = sqlType;
+        using var context = new TestContext(ContextUtil.GetOptions());
+
+        var entities = new List<TrayType> { new TrayType() };
+        var info = TableInfo.CreateInstance(context, null, entities, OperationType.Insert, new BulkConfig
+        {
+            SetOutputIdentity = true
+        });
+
+        Assert.NotNull(info);
+    }
+}
