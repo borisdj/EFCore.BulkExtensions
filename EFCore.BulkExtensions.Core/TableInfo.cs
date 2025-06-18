c4fb8365a6c7e2db96ac04f8e76b8fa0a4d79d92
Nico Kranz <nico.kranz@esh.essen.de>
2025-06-18 13:38:06 +0200

    include IEntityTypes ViewName if present

--- EFCore.BulkExtensions.Core/TableInfo.cs
+++ EFCore.BulkExtensions.Core/TableInfo.cs
@@ -198,6 +198,8 @@ public class TableInfo
         Schema = customTableName != null ? customSchema : entityType.GetSchema() ?? defaultSchema;
 
         var entityTableName = entityType.GetTableName();
+        var entityViewName = entityType.GetViewName();
+        bool isView = entityTableName == null && entityViewName != null;
         TableName = customTableName ?? entityTableName;
 
         string? sourceSchema = null;
@@ -224,12 +226,14 @@ public class TableInfo
         }
         TempTableName = sourceTableName ?? $"{TableName}{TempTableSufix}";
 
-        if (entityTableName is null)
+        if ((!isView && entityTableName is null) || (isView && entityViewName is null))
         {
-            throw new ArgumentException("Entity does not contain a table name");
+            throw new ArgumentException("Entity does not contain a table or view name");
         }
 
-        ObjectIdentifier = StoreObjectIdentifier.Table(entityTableName, entityType.GetSchema());
+        ObjectIdentifier = isView 
+            ? StoreObjectIdentifier.View(entityViewName!, entityType.GetSchema()) 
+            : StoreObjectIdentifier.Table(entityTableName!, entityType.GetSchema());
 
         var allProperties = new List<IProperty>();
         foreach (var entityProperty in entityType.GetProperties())
